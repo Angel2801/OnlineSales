1ad77d6d984ef12170000faa4bc5c52318a3ac54
Angel2801 <21bce017@nirmauni.ac.in>
2024-09-12 16:45:01 +0530

    3.0

--- utils/LogReader.py
+++ utils/LogReader.py
@@ -10,43 +10,32 @@ class LogReader:
         self.__sortLogs()
 
     def __readLogs(self, filepath):
-        # Open and load the JSON file
         with open(filepath, "r") as file:
             logs = json.load(file)
         return logs
 
     def displayLogs(self):
-        # Display each log in the buffer
         for log in self.buffer:
             print(log)
 
     def __getLogs(self):
-        # Print the logs to understand the structure
-        print("Logs loaded from file:")
-        print(self.logs)
-
-        # Iterate through each dictionary in the list
         for log_collection in self.logs:
-            # Each log_collection is a dictionary of logs keyed by microservice names
             for microservice_name, log_details in log_collection.items():
                 try:
-                    # Print log details to help with debugging
-                    print("Processing log entry:", log_details)
-
-                    # Parse the date and time into a datetime object
-                    date_time_str = f"{log_details['date']} {
-                        log_details['time']}"
+                    time_str = log_details["time"].split(" ")[
+                        0
+                    ] 
+                    date_time_str = f"{log_details['date']} {time_str}"
                     parsed_datetime = datetime.strptime(
-                        date_time_str, "%a %B %d %Y %H:%M:%S %Z"
+                        date_time_str, "%a %B %d %Y %H:%M:%S"
                     )
 
-                    # Separate date and time
                     log_date = parsed_datetime.date()
                     log_time = parsed_datetime.time()
 
                     transformed_log = {
-                        "date": log_date,  # Store date part separately
-                        "time": log_time,  # Store time part separately
+                        "date": log_date,  
+                        "time": log_time,  
                         "microservice_name": microservice_name,
                         "log_type": log_details["log_type"],
                     }
@@ -59,5 +48,7 @@ class LogReader:
 
     def __sortLogs(self):
         self.__getLogs()
-        # Sort the buffer by the 'date' and 'time' fields
         self.buffer.sort(key=lambda x: (x["date"], x["time"]))
+
+log_reader = LogReader()
+log_reader.displayLogs()
